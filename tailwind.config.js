1fe746b117c535dc86e5588beac41c14b6b311ba
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 00:34:57 -1100

    :construction: Header && Mininav
    completed header and mobileNav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      maven: ["Maven Pro", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
