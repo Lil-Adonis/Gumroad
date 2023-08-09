8cb7d22102bd12cee6e0065e5a45df688dbffc4b
Lil-Adonis <adenugaib24@gmail.com>
2023-08-09 03:03:19 -1100

    Discover component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from "./components/Header";
 import Hero from './components/Hero';
+import Discover from './components/discover/Discover';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
    <div className=''>
     <Header/>
     <Hero/>
+    <Discover/>
    </div>
   )
 }
